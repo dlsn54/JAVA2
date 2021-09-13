aa028ddb974b9d5a3af199bbe3e443e6d32dd59a
Mac_1 <Mac_1@DESKTOP-P3QHQ70>
2021-09-13 13:39:25 +0900

    11

---
+++ Array/src/CommandLine.java
@@ -0,0 +1,17 @@
+import java.util.Iterator;
+
+public class CommandLine {
+
+	public static void main(String[] args) {
+		if(args.length>0) {
+			for (int i = 0; i < args.length; i++) {
+				System.out.println(args[i]);
+			}
+		}
+		if(args[0].equals("-version"))
+		{
+			System.out.println("버전을 확인하고 있네요");
+		}
+	}
+
+}
