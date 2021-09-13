02b223e4f2a843401d7a72b20f24bf7036e38fdc
Mac_1 <Mac_1@DESKTOP-P3QHQ70>
2021-09-13 13:42:44 +0900

    test

--- First/src/Add2.java
+++ First/src/Add2.java
@@ -5,9 +5,9 @@ public class Add2{
 		Scanner input = new Scanner(System.in);
 		int x,y,sum;
 		
-		System.out.print("첫 번째 수 입력2 : ");
+		System.out.print("첫 번째 수 입력22 : ");
 		x=input.nextInt();
-		System.out.print("두 번째 수 입력2 : ");
+		System.out.print("두 번째 수 입력22 : ");
 		y=input.nextInt();
 		
 		sum=x+y;
